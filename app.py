291406b086d74ead86a1f42e99bf87e52c14a858
GiftsonJe <154898159+GiftsonJe@users.noreply.github.com>
2023-12-26 20:05:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('calculator.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    expression = request.form['expression']
+    result = eval(expression)
+    return render_template('calculator.html', result=result, expression=expression)
+
+if __name__ == '__main__':
+    app.run(debug=True)
